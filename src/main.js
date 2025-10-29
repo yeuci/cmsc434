d62d85a59888e34e14a97460e232d047edb8c92c
neb <nebbbnn@gmail.com>
2025-10-29 18:05:05 -0400

    small styling

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const pages = document.querySelectorAll(".page");
   const navs = document.querySelectorAll(".navigation .nav");
 
+  navs[0].classList.add("active");
+
   const targetByIndex = [
     "inventory--page",
     "shopping-lists--page",
@@ -15,6 +17,12 @@ document.addEventListener("DOMContentLoaded", () => {
     pages.forEach((p) =>
       p.classList.toggle("active-page", p.classList.contains(targetClass))
     );
+
+    // might come back to... nav bg
+    navs.forEach((nav, i) => {
+      const target = targetByIndex[i];
+      nav.classList.toggle("active", target === targetClass);
+    });
   }
 
   navs.forEach((nav, i) => {
